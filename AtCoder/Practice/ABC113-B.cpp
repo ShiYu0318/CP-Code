de588b9dd052a0270c08b7a0563a7e26536fbe8f
士育 <huangshiyu0318@gmail.com>
2023-11-12 23:44:53 +0800

    2023/11/13

---
+++ AtCoder/Practice/ABC113-B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ShiYu ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+#define FOR(i,n) for(int i=0;i<n;++i)
+
+signed main()
+{
+    ShiYu
+    double n,t,a; cin >> n >> t >> a;
+    double h,minn = INT_MAX,ans;
+    FOR(i,n)
+    {
+        cin >> h;
+        if(abs((t - h * 0.006) - a) < minn)
+        {
+            minn = abs((t - h * 0.006) - a);
+            ans = i+1;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}

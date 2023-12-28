5eccb32db796b137f13b6bcc901aa883964a2983
士育 <huangshiyu0318@gmail.com>
2023-12-28 17:02:59 +0800

    2023/12/27

--- CP_template.cpp
+++ CP_template.cpp
@@ -3,44 +3,43 @@
 #define ll long long
 #define ull unsigned ll
 #define int ll
-#define vii vector<pair<int, int>>
+#define vii vector< pair<int, int> >
 #define pq priority_queue
 #define pq_min priority_queue < int, vector<int>, greater<int> > 
+#define F first
+#define S second
+#define pb push_back
+#define mp(a,b) make_pair(a,b)
+#define sz(x) ((ll)x.size())
+#define all(x) x.begin(), x.end()
 #define MMn maxn = INT_MIN, minn = INT_MAX
 #define max(a, b) (a > b ? a : b)
 #define min(a, b) (a < b ? a : b)
 #define remax(a, b) a = (a > b ? a : b)
 #define remin(a, b) a = (a < b ? a : b)
-#define sz(x) ((ll)x.size())
-#define all(x) x.begin(), x.end()
-#define mp(a,b) make_pair(a,b)
+#define coutE(x) cout << x << "\n"
+#define coutS(x) cout << x << ' '
+#define dbg(x) cerr << #x << "=" << x << "\n";
 #define FOR(i,a,b) for(int i=a;i<b;++i)
 #define FORn(i,n) for(int i=0;i<n;++i)
 #define rFOR(i,n) for(int i=n-1;i>=0;--i)
-#define coutE(x) cout << x << "\n"
-#define coutS(x) cout << x << ' '
 #define input(x) for(auto &i : x) cin >> i
-#define output(x) for(auto &i : x) coutS(i)
-#define inputD(x,a,b) FOR(i,0,a) FOR(j,0,b) cin >> x[i][j]
-#define dbg(x) cout << #x << "=" << x << " ";
-#define DB(x) cerr << #x << " " << x << endl
+#define inputn(x,n) FORn(i,n) cin >> x[i] 
+#define inputD(x,a,b) FORn(i,a) FORn(j,b) cin >> x[i][j]
+#define output(x) for(auto &i : x) dbg(i)
 #define YN(x) cout << (x ? "YES" : "NO") << "\n"
 #define Yn(x) cout << (x ? "Yes" : "No") << "\n"
 #define yn(x) cout << (x ? "yes" : "no") << "\n"
 #define SET(n) cout << fixed << setprecision(n)
-#define F first
-#define S second
-#define pb push_back
-#define nl cout << "\n"
+#define nl(n) FORn(i,n) cout << "\n"
 const double eps = 1e-8;
 using namespace std;
 
 //==========================================================================================
 
 void solve()
-{	
-
-
+{
+    
 }
 
 signed main(void)
--- P_template.cpp
+++ P_template.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #define FOR(i,a,b) for(int i=a;i<b;++i)
 #define FORn(i,n) for(int i=0;i<n;++i)
-#define rFORn(i,n) for(int i=n-1;i>=0;--i)
+#define rFOR(i,n) for(int i=n-1;i>=0;--i)
 #define input(x) for(auto &i : x) cin >> i
 #define output(x) for(auto &i : x) cout << i << " "
 #define inputD(x,a,b) FORn(i,a) FORn(j,b) cin >> x[i][j]

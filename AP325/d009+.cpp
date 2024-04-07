2489fb1842fbfa932329ffe84f38c45af330e1c1
士育 <huangshiyu0318@gmail.com>
2024-04-07 23:17:05 +0800

    20240407

---
+++ AP325/d009+.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ShiYu ios_base::sync_with_stdio(0),cin.tie(0);
+#define FOR(i,a,b) for(int i=a;i<b;++i)
+#define rFOR(i,n) for(int i=n-1;i>=0;--i)
+#define RPT(i,n) FOR(i,0,n)
+#define input(x) for(auto &i : x) cin >> i
+#define output(x) for(auto i : x) cout << i << " "
+#define Yn(x) cout << (x ? "Yes" : "No") << "\n"
+#define all(x) x.begin(),x.end()
+#define int long long
+#define pii pair<int,int>
+#define F first
+#define S second
+
+// 遞迴
+
+int tb[13][13];
+
+// remove frame
+int rmf(int ax, int ay, int bx, int by)
+{
+    if(ax == bx || ay == by) return 0;
+    int u = 0, d = 0, l = 0, r = 0;
+    FOR(j,ay,by+1)
+    {
+        u += tb[ax][j];
+        d += tb[bx][j];
+    }
+    FOR(i,ax,bx+1)
+    {
+        l += tb[i][ay];
+        r += tb[i][by];
+    }
+    int m = by - ay + 1,
+        n = bx - ax + 1;
+    u = min(u, m - u);
+    d = min(d, m - d);
+    l = min(l, n - l);
+    r = min(r, n - r);
+    int w = u + rmf(ax+1, ay, bx, by),
+        x = d + rmf(ax, ay, bx-1, by),
+        y = l + rmf(ax, ay+1, bx, by),
+        z = r + rmf(ax, ay, bx, by-1);
+    return min({w,x,y,z});
+}
+
+signed main()
+{
+    ShiYu
+    int m, n; cin >> m >> n;
+    RPT(i,m) RPT(j,n) cin >> tb[i][j];
+    cout << rmf(0,0,m-1,n-1) << "\n";
+}
--- test.cpp
+++ test.cpp
@@ -3,17 +3,6 @@ using namespace std;
 
 int main()
 {
-    // 如何輸出一串數字用空格分開並在最後換行
-
-    // 方法一
-    for(int i=0; i<n; ++i){
-        cout << i << " ";
-    }
-    cout << "\n";
-
-    // 方法二
-    for(int i=0; i<n; ++i){
-        cout << i << " \n"[i == n-1];
-    }
+    cout << min({6,4,5,8,8,4,1});
 
 }

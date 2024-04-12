de39a062dc03b51cbf8c082117da6f049c4095b4
士育 <huangshiyu0318@gmail.com>
2024-04-12 10:45:52 +0800

    20240412

--- Kattis/Basic/Fifty shades of pink.cpp
+++ Kattis/Basic/Fifty shades of pink.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define int long long
-#define IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ShiYu ios_base::sync_with_stdio(0); cin.tie(0)
 using namespace std;
 
 signed main()

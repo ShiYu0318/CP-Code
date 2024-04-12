6327c82cc1888b4cae45bcd9aa9322537637bcbc
士育 <huangshiyu0318@gmail.com>
2024-04-12 10:19:29 +0800

    20240412

--- CP Template.cpp
+++ CP Template.cpp
@@ -1,58 +1,70 @@
 #include <bits/stdc++.h>
-#define ShiYu ios_base::sync_with_stdio(0),cin.tie(0)
+#define ShiYu ios_base::sync_with_stdio(0); cin.tie(0)
+#pragma GCC optimize("Ofast")
+using namespace std;
+// type --------------------------------------------------------------------
 #define int long long
 #define ull unsigned long long
 #define pii pair<int,int>
-#define vii vector<pii>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
 #define pq priority_queue
-#define pq_min priority_queue<int,vector<int>,greater<int>> 
+#define pq_min priority_queue<int,vector<int>,greater<int>>
+#define MM mx = INT_MIN, mi = INT_MAX
+// simple ------------------------------------------------------------------
 #define F first
 #define S second
+#define MP make_pair
 #define EB emplace_back
-#define MP(a,b) make_pair(a,b)
-#define SZ(x) ((ll)x.size())
+// function ----------------------------------------------------------------
+#define SZ(x) (int)x.size()
 #define all(x) x.begin(), x.end()
-#define MMn maxn = INT_MIN, minn = INT_MAX
-#define max(a, b) (a > b ? a : b)
-#define min(a, b) (a < b ? a : b)
-#define remax(a, b) a = (a > b ? a : b)
-#define remin(a, b) a = (a < b ? a : b)
-#define coutN(x) cout << x << "\n"
-#define coutS(x) cout << x << ' '
-#define dbg(x) cerr << #x << "=" << x << "\n";
-#define FOR(i,a,b) for(int i=a;i<b;++i)
-#define rFOR(i,n) for(int i=n-1;i>=0;--i)
-#define RPT(i,n) FOR(i,0,n)
-#define input(x) for(auto &i : x) cin >> i
-#define inputn(x,n) RPT(i,n) cin >> x[i] 
-#define inputD(x,a,b) RPT(i,a) RPT(j,b) cin >> x[i][j]
-#define output(x) for(auto i : x) cout << i << " ";cout << '\n'
-#define outputn(x,n) RPT(i,n) cout << x[i] << " \n"[i == n-1];
-#define outputD(x,m,n) RPT(i,m) RPT(j,n) cout << x[i][j] << " \n"[j == n-1];
-#define YN(x) cout << (x ? "YES" : "NO") << "\n"
-#define Yn(x) cout << (x ? "Yes" : "No") << "\n"
-#define yn(x) cout << (x ? "yes" : "no") << "\n"
-#define islower(x) ('a' >= (x) && (x) <= 'z')
-#define isupper(x) ('A' >= (x) && (x) <= 'Z')
+#define sortall(x) sort(x.begin(), x.end())
+#define remax(a,b) a = (a > b ? a : b)
+#define remin(a,b) a = (a < b ? a : b)
+#define islower(x) ('a' >= x && x <= 'z')
+#define isupper(x) ('A' >= x && x <= 'Z')
 #define isletter(x) (islower(x) || isupper(x))
 #define SET(n) cout << fixed << setprecision(n)
-#define nl(n) RPT(i,n) cout << "\n"
+// loop --------------------------------------------------------------------
+#define RPT(i,n) for(int i=0; i<n; ++i)
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define rFOR(i,n) for(int i=n-1; i>=0; --i)
+#define forit(it,x) for (auto it = x.begin(); it != x.end(); ++it)
+// IO ----------------------------------------------------------------------
+#define sp << ' ' << 
+#define nl cout << '\n'
+#define coutS(x) cout << x << ' '
+#define coutN(x) cout << x << '\n'
+#define NL(n) RPT(i,n) cout << '\n'
+#define dbg(x) cerr << #x << " = " << x << '\n';
+// loop IO -----------------------------------------------------------------
+#define input(x) for(auto &i:x) cin >> i
+#define inputN(x,n) RPT(i,n) cin >> x[i]
+#define inputD(x,a,b) RPT(i,a) RPT(j,b) cin >> x[i][j]
+#define output(x) for(auto i:x) cout << i << ' '; cout << '\n'
+#define outputN(x,n) RPT(i,n) cout << x[i] << " \n"[i == n-1];
+#define outputD(x,a,b) RPT(i,a) RPT(j,b) cout << x[i][j] << " \n"[j == n-1]
+// YN ----------------------------------------------------------------------
+#define YN(x) cout << (x ? "YES" : "NO") << '\n'
+#define Yn(x) cout << (x ? "Yes" : "No") << '\n'
+#define yn(x) cout << (x ? "yes" : "no") << '\n'
+// const -------------------------------------------------------------------
 const double eps = 1e-8;
-using namespace std;
 
+// ==========================================================================================
 // 
-
-//==========================================================================================
+// ==========================================================================================
 
 void solve()
 {
-    int n; cin >> n;
-	
+	int n; cin >> n;
 }
 
-signed main(void)
+signed main()
 {
 	ShiYu;
-	int t; cin >> t;
+	int t = 1;
+	cin >> t; // 單筆測資時註解
 	while(t--) solve();
 }
--- P Template.cpp
+++ P Template.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define ShiYu ios_base::sync_with_stdio(0),cin.tie(0);
-#define FOR(i,a,b) for(int i=a;i<b;++i)
-#define rFOR(i,n) for(int i=n-1;i>=0;--i)
-#define RPT(i,n) FOR(i,0,n)
-#define input(x) for(auto &i : x) cin >> i
-#define output(x) for(auto i : x) cout << i << " "
-#define Yn(x) cout << (x ? "Yes" : "No") << "\n";
-#define all(x) x.begin(),x.end()
+#define ShiYu ios_base::sync_with_stdio(0); cin.tie(0)
 #define int long long
+#define vi vector<int>
 #define pii pair<int,int>
 #define F first
 #define S second
+#define EB emplace_back
+#define nl cout << '\n'
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define remax(a,b) a = (a > b ? a : b)
+#define remin(a,b) a = (a < b ? a : b)
+#define RPT(i,n) for(int i=0; i<n; ++i)
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define rFOR(i,n) for(int i=n-1; i>=0; --i)
+#define input(x) for(auto &i:x) cin >> i
+#define output(x) for(auto i:x) cout << i << ' '; nl
+#define outputN(x,n) RPT(i,n) cout << x[i] << " \n"[i == n-1];
+#define Yn(x) cout << (x ? "Yes" : "No") << '\n';
+using namespace std;
 
 signed main()
 {
-    ShiYu
+    ShiYu;
     
 }

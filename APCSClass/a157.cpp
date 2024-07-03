529bf6a8ea210cd0e3088cacf991019b75c1367f
士育 <huangshiyu0318@gmail.com>
2024-07-03 19:00:27 +0800

    20240703

---
+++ APCSClass/a157.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ShiYu ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define F first
+#define S second
+#define EB emplace_back
+#define endl '\n'
+#define nl cout << '\n'
+#define SZ(x) ((int)x.size())
+#define all(x) x.begin(), x.end()
+#define RPT(i,n) for(int i=0; i<n; ++i)
+#define FOR(i,a,b) for(int i=a; i<b; ++i)
+#define rFOR(i,n) for(int i=n-1; i>=0; --i)
+#define input(x) for(auto &i:x) cin >> i
+#define output(x) for(auto i:x) cout << i << ' '; nl
+#define outputN(x,n) RPT(i,n) cout << x[i] << " \n"[i == n-1];
+#define Yn(x) cout << (x ? "Yes" : "No") << '\n';
+
+int f(int n)
+{
+    if(n <= 1) return n;
+    else return f(n - 1) + f(n - 2);
+}
+
+signed main()
+{
+    ShiYu;
+    int n;
+    while(cin >> n)
+    {
+        cout << f(n) << '\n';
+    }
+}

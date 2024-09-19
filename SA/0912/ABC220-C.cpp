1b0e3004f4856dd5648164b6714d28c2db075471
士育 <huangshiyu0318@gmail.com>
2024-09-20 04:26:49 +0800

    20240919

---
+++ SA/0912/ABC220-C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll n; cin >> n;
+    vector<ll> v(n); for(auto &i:v) cin >> i;
+    ll x; cin >> x;
+
+    ll sum = 0;
+    for(ll i=0; i<n; ++i)
+    {
+        sum += v[i];
+        if(sum > x)
+        {
+            cout << i+1 << "\n";
+            return 0;
+        }
+    }
+    ll rnd = x / sum, sumb = rnd * sum, ans = rnd * n;
+    for(int i=0; i<n; ++i)
+    {
+        sumb += v[i];
+        ans++;
+        if(sumb > x)
+        {
+            cout << ans << "\n";
+            return 0;
+        }
+    }
+}
---
+++ SA/0912/ABC353-C.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+    int n; cin >> n;
+    vector<int> v(n);
+    long long sum = 0, cnt = 0;
+    for(auto &i:v)
+    {
+        cin >> i;
+        sum += i;
+    }
+    sum *= n-1;
+    sort(v.begin(), v.end());
+    for(int l=0,r=n; l<n; ++l)
+    {
+        r = max(l+1, r);
+        while(l < r-1 && v[l] + v[r-1] >= 1e8) r--;
+        cnt += n - r;
+    }
+    cout << sum - cnt * 100000000 << "\n";
+}

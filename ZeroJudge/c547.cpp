a8b3376a35264f3168878159e33c6bf66d42fd37
士育 <huangshiyu0318@gmail.com>
2024-10-15 01:16:57 +0800

    20241015

--- ZeroJudge/c547.cpp
+++ ZeroJudge/c547.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1000000007;
+const int MOD = 1e9+7, N = 10005;
+int dp[N];
 
 int main()
 {
-    int n, dp[10005];
     dp[0] = dp[1] = 1;
+    for(int i=2; i<=N; i++)
+    {
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+    }
+    
+    int n;
     while(cin >> n)
     {
-        for(int i=2; i<=n; i++)
-        {
-            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
-        }
         cout << dp[n] << "\n";
     }
 }
--- test.cpp
+++ test.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    int n, t, mi = 1e9, mx = -1e9; cin >> n;
+    while(n--)
+    {
+        cin >> t;
+        mx = max(mx, t);
+        mi = min(mi, t);
+    }
+    cout << mi << ' ' << mx << ' ' << (long long)mi * mx << '\n';
 }

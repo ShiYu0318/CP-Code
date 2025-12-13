baba65320eec1891a99aa0df5ae68eef2ab46162
士育 <huangshiyu0318@gmail.com>
2025-12-13 08:59:10 +0800

    20251213

--- -HW/FINAL/A01-114502540.cpp
+++ -HW/FINAL/A01-114502540.cpp
@@ -10,38 +10,10 @@ int main()
         for(int i=0; i<N; ++i)
         {
             string op; cin >> op;
-            if(op == "north")
-            {
-                tmp = t;
-                t = s;
-                s = d;
-                d = n;
-                n = tmp;
-            }
-            else if(op == "south")
-            {
-                tmp = t;
-                t = n;
-                n = d;
-                d = s;
-                s = tmp;
-            }
-            else if(op == "west")
-            {
-                tmp = t;
-                t = e;
-                e = d;
-                d = w;
-                w = tmp;
-            }
-            else
-            {
-                tmp = t;
-                t = w;
-                w = d;
-                d = e;
-                e = tmp;
-            }
+            if(op == "north") { tmp = t; t = s; s = d; d = n; n = tmp; }
+            else if(op == "south") { tmp = t; t = n; n = d; d = s; s = tmp; }
+            else if(op == "west") { tmp = t; t = e; e = d; d = w; w = tmp; }
+            else { tmp = t; t = w; w = d; d = e; e = tmp; }
         }
         cout << t << "\n";
     }

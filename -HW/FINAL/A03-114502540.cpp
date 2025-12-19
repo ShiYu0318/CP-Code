d9776a9125919417d403c92e18743bb2f51dab09
士育 <huangshiyu0318@gmail.com>
2025-12-19 21:49:12 +0800

    20251219

--- -HW/FINAL/A03-114502540.cpp
+++ -HW/FINAL/A03-114502540.cpp
@@ -5,8 +5,7 @@ const int MN = 15;
 
 int main()
 {
-    // 定義 dp[x][y][z] = 走到 x,y 的方法數 z = 1 上一步是往下
-    // 轉移：
+    // 定義 dp[x][y][z] = 走到 x,y 的方法數, z = 1:上一步是往下
     int m,n; cin >> m >> n;
     int dp[MN][MN][2] = {0};
     for(int i=0; i <= m; ++i)
@@ -17,13 +16,12 @@ int main()
             else if(i == 0 && j == 1) dp[i][j][0] = 1;
             else if(i == 1 && j == 0) dp[i][j][1] = 1;
             else
-            {
+            {   // 轉移
                 dp[i][j][0] += dp[i][j-1][0]  + dp[i][j-1][1];
                 if(j >= 2) dp[i][j][0] += dp[i][j-2][1] + dp[i][j-2][0];
                 if(i != 0) dp[i][j][1] += dp[i-1][j][0];
             }
         }
     }
-
     cout << dp[m][n][0] + dp[m][n][1] << "\n";
 }

9b29c4635988c9dd8f505a183f4558e18fbe1053
士育 <huangshiyu0318@gmail.com>
2026-01-02 14:32:30 +0800

    20260102

---
+++ LeetCode/66.cpp
@@ -0,0 +1,32 @@
+// 2025/01/01
+// 3ms
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1]++;
+        for (int i = digits.size() - 1; i > 0; --i) {
+            digits[i - 1] += digits[i] / 10;
+            digits[i] %= 10;
+        }
+        if (digits[0] >= 10) {
+            digits.emplace_back(0);
+            for (int i = digits.size() - 1; i > 1; --i)
+                digits[i] = digits[i - 1];
+            digits[1] = digits[0] % 10;
+            digits[0] = 1;
+        }
+        return digits;
+    }
+};
+
+// 0ms
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int i=digits.size()-1;
+//         while(i >= 0 && digits[i] == 9) digits[i--] = 0;
+//         if(i < 0) digits.insert(digits.begin(),1);
+//         else digits[i]++;
+//         return digits;
+//     }
+// };
---
+++ LeetCode/961.cpp
@@ -0,0 +1,13 @@
+// 20250102 0ms
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        bitset<10005> bs;
+        for(auto i : nums)
+        {
+            if(bs[i]) return i;
+            else bs[i] = 1;
+        }
+        return 0;
+    }
+};

1327d6b62abf1097d96438a6991da3ced2f034fd
士育 <huangshiyu0318@gmail.com>
2026-02-26 14:31:21 +0800

    20260226

--- LeetCode/1. Easy/1022.cpp
+++ LeetCode/1. Easy/1022.cpp
@@ -11,25 +11,43 @@
  */
 class Solution {
     // 20260226
+    // O(N)
 public:
-    int btd(string s)
-    {
-        int num = 0;
-        for(int i = s.size()-1, k = 0; i >= 0; --i, ++k) num += (s[i] == '1' ? (1 << k) : 0);
-        return num;
-    }
-
-    int DFS(TreeNode* node, string s)
+    int DFS(TreeNode* node, int sum)
     {
         if(!node) return 0;
-        s += char('0' + node->val);
-        if(!node->left && !node->right) return btd(s);
-        int num = 0;
-        num += DFS(node->left, s) + DFS(node->right, s);
-        return num;
+        sum = sum * 2 + node->val;
+        if(!node->left && !node->right) return sum;
+        return DFS(node->left, sum) + DFS(node->right, sum);
     }
 
     int sumRootToLeaf(TreeNode* root) {
-        return DFS(root, "");
+        return DFS(root, 0);
     }
 };
+
+// class Solution {
+//     // 20260226
+//     // O(N*H)  H := 樹高
+// public:
+//     int btd(string s)
+//     {
+//         int num = 0;
+//         for(int i = s.size()-1, k = 0; i >= 0; --i, ++k) num += (s[i] == '1' ? (1 << k) : 0);
+//         return num;
+//     }
+
+//     int DFS(TreeNode* node, string s)
+//     {
+//         if(!node) return 0;
+//         s += char('0' + node->val);
+//         if(!node->left && !node->right) return btd(s);
+//         int num = 0;
+//         num += DFS(node->left, s) + DFS(node->right, s);
+//         return num;
+//     }
+
+//     int sumRootToLeaf(TreeNode* root) {
+//         return DFS(root, "");
+//     }
+// };
